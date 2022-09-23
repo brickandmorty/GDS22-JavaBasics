27707cb3eca697c87f2ea41bb15ac606cccf137e
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-09-23 17:10:17 +0200

    my first class created, args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+
+    // wir haben hiermit unsere erste Klasse erstellt
+
+    //psvm + TAB
+    public static void main(String[] args) {
+        // hier machen wir eine erste Ausgabe:
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        // args
+        // sout + TAB
+        System.out.println(Arrays.toString(args));
+    }
+
+}
